91d06267e220af63b3a74be4285edfa7f9e19c0d
AlekseiBogdan <apbkgdan@gmail.com>
2023-11-19 12:29:22 +0300

    task_1

--- lesson_1/task_1.php
+++ lesson_1/task_1.php
@@ -1,7 +1,16 @@
 <?php 
 
 echo "
-<div>hey!</div>
-"
+<div style='width: 525px; margin: auto; background-color: lightgrey;'>
+    <p style='text-align: center;'>Внимание!</p>
+    <span>Мы становимся профессиональными Web-программистами. Изучив язык PHP, мы сможем создавать:</span>
+    <ul>
+        <li>гостевые книги,</li>
+        <li>форумы,</li>
+        <li>чаты</li>
+        <li>и управлять целыми портальными системами. Мы сделаем это!</li>
+    </ul>
+</div>
+";
 
 ?>
