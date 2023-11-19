2e79492eac343ed227893134d2dd47d21cc8c89a
AlekseiBogdan <apbkgdan@gmail.com>
2023-11-19 12:42:33 +0300

    task_2

---
+++ lesson_1/task_2.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = 3;
+
+if ($a == 1 || $a == 2) {
+    echo 'Hello world';
+} elseif ($a == 3) {
+    echo 'Hi world';
+} else {
+    echo 'Goodbye world';
+}
+
+// switch($a){ 
+//     case 1:
+//     case 2:
+//     echo 'Hello world';
+//     break;
+//     case 3:
+//     echo 'Hi world';
+//     break;
+//     default:
+//     echo 'Goodbye world';
+//     }
+    
+
+?>
