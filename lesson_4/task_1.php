a9bd7a5ec5b6f61569db93d1ef4d7046049a6ce5
AlekseiBogdan <apbkgdan@gmail.com>
2023-12-02 17:23:55 +0300

    lesson_4

---
+++ lesson_4/task_1.php
@@ -0,0 +1,19 @@
+﻿<?php
+    // function foo(){
+    //     function bar(){
+    //         echo "Hello world";
+    //     }
+    // } bar();
+
+    function foo(){
+        function bar(){
+            echo "Hello world";
+        }
+    }
+    
+    foo();
+    bar();
+
+    // выполнится второй вариант, так как есть необходимость инициализировать функцию bar(), без foo() ее не будет существовать
+?>
+
---
+++ lesson_4/task_3.php
@@ -0,0 +1,19 @@
+﻿<?php
+
+$test = 5;
+echo $test.'<br>';
+
+function toHello($input) {
+    $input = "Hello world <br>";
+    return $input;
+}
+
+echo toHello($test);
+
+
+// toHello(1);
+// toHello("Hey");
+// toHello([1,2]);
+
+?>
+
