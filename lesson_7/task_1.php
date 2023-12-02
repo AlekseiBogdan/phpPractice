96c340602a7e85d7662942258ae29386b1b99cb9
AlekseiBogdan <apbkgdan@gmail.com>
2023-12-02 23:12:25 +0300

    lesson_7

---
+++ lesson_7/task_1.php
@@ -0,0 +1,50 @@
+﻿<?php
+
+class Student {
+    private $name;
+    private $age;
+    private $group;
+
+    public function setName($newName) {
+        $this->$name = $newName;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAge($newAge) {
+        $this->age = $newAge;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setGroup($newGroup) {
+        $this->group = $newGroup;
+    }
+
+    public function getGroup() {
+        return $this->group.'<br/>';
+    }
+}
+
+
+$firstStudent = new Student;
+
+$secondStudent = new Student;
+
+$firstStudent->setName('Иван');
+$firstStudent->setAge(21);
+$firstStudent->setGroup('U1001');
+
+
+$secondStudent->setName('Вася');
+$secondStudent->setAge(20);
+$secondStudent->setGroup('U1002');
+
+echo $firstStudent->getGroup();
+echo $secondStudent->getGroup();
+echo $firstStudent->getAge() + $secondStudent->getAge();
+?>
---
+++ lesson_7/task_2.php
@@ -0,0 +1,70 @@
+﻿<?php
+
+class User {
+    protected $name;
+    protected $age;
+    
+    public function setName($newName) {
+        $this->name = $newName;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setAge($newAge) {
+        $this->age = $newAge;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+}
+
+class Worker extends User {
+    private $salary;
+
+    public function setSalary($newSalary) {
+        $this->salary = $newSalary;
+    }
+
+    public function getSalary() {
+        return $this->salary;
+    }
+}
+
+$firstWorker = new Worker;
+$secondWorker = new Worker;
+
+$firstWorker->setName('Иван');
+$firstWorker->setAge(25);
+$firstWorker->setSalary(1000);
+
+$secondWorker->setName('Вася');
+$secondWorker->setAge(26);
+$secondWorker->setSalary(2000);
+
+echo $firstWorker->getSalary() + $secondWorker->getSalary();
+
+
+class Student extends User {
+    private $grant; //scholarship не подходит для описания стипендии в нашем привычном понимании :), из моих поисков я понял, что наилучшим будет или grant или student allowance 
+    private $educationYear;
+
+    public function setGrant($newGrant) {
+        $this->grant = $newGrant;
+    }
+
+    public function getGrant() {
+        return $this->grant;
+    }
+
+    public function setYear($newYear) {
+        $this->educationYear = $newYear;
+    }
+
+    public function getYear() {
+        return $this->educationYear;
+    }
+}
+?>
