e91303f0d660bf49590bae0eb75188030e8d9017
AlekseiBogdan <apbkgdan@gmail.com>
2023-12-02 21:10:39 +0300

    lesson_6

---
+++ lesson_6/session2.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+session_start();
+
+echo 'Здравствуйте, '.$_SESSION['username'].'<br>';
+
+echo 'your pass '.$_SESSION['pass'].'<br>';
+
+echo "<a href='session3.php'>Следующая страница</a>";
+
+?>
---
+++ lesson_6/session3.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+session_start();
+// Разрегистрировали переменную
+unset($_SESSION['username']);
+unset($_SESSION['pass']);
+// Разрушаем сессию
+session_destroy(); 
+
+
+?>
