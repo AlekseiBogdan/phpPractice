ee372aeb28f4addcc5da97e23a5b0869a3cf4a74
AlekseiBogdan <apbkgdan@gmail.com>
2023-12-02 19:05:38 +0300

    lesson_5

---
+++ lesson_5/answer.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+$pres = $_POST['pres'];
+$year = $_POST['year'];
+
+switch ($pres) {
+    case 'bush':
+        echo 'Вы ответили — Буш, правильно — Путин'.'<br>';
+        break;
+    case 'putin':
+        echo 'Верно!'.'<br>';
+        break;
+    case 'medv':
+        echo 'Вы ответили — Медведев, правильно — Путин'.'<br>';
+        break;
+}
+
+switch ($year) {
+    case 365:
+        echo 'Вы ответили — '.$year.', правильно — 366'.'<br>';
+        break;
+    case 366:
+        echo 'Верно!';
+        break;
+    case 369:
+        echo 'Вы ответили — '.$year.', правильно — 366'.'<br>';
+        break;
+}
+
+?>
---
+++ lesson_5/form.php
@@ -0,0 +1,13 @@
+﻿<form action='answer.php' method='POST' name='form'>
+    <p>Кто явялется президентом России?</p>
+    <input type=radio name='pres' value='bush'>Буш</input><br>
+    <input type=radio name='pres' value='putin'>Путин</input><br>
+    <input type=radio name='pres' value='medv'>Медведев</input><br>
+
+    <p>Сколько дней в високосном году?</p>
+    <input type=radio name='year' value=365>365</input><br>
+    <input type=radio name='year' value=366>366</input><br>
+    <input type=radio name='year' value=369>369</input><br>
+
+    <button name='submit' type='submit'>Отправить ответы</button>
+</form>
